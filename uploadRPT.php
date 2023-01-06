6913ab8ab00e960fb9731d471a94c44e7850f999
Adeleke Ojora <adeleke.ojora@firs.gov.ng>
2023-01-06 18:44:33 +0100

    temporary commit

--- uploadRPT.php
+++ uploadRPT.php
@@ -11,66 +11,74 @@ if (isset($_POST["upload"])) {
   try {
     $db = new connectDatabase();
     $filename = $_FILES["file"]["tmp_name"];
-    if ($_FILES["file"]["size"] > 0) {
-      $tot = count(file($filename, FILE_SKIP_EMPTY_LINES));
-      $file = fopen($filename, "r");
 
-      while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
-        $fnm = getFirstName($emapData[0]);
-        $phn = $emapData[1];
-        $wrd = $emapData[2];
-        $lga = $emapData[3];
+    if ($_FILES["file"]["size"] > 0 && $_FILES["file"]["type"] == 'text/csv') {
+      if (($file = fopen($filename, "r")) !== FALSE) {
+        $file = file($filename,FILE_SKIP_EMPTY_LINES);
+        // $tot = count($file);
+        die(var_dump(count($file)));
 
-        $cCrt = canCreate($phn, $db);
-        if (!$cCrt) {
-          $msg.='Duplicate Phonenumber (<b>'.$phn.'</b>)!<br>';
-        } else {
-          if ($db->isLastQuerySuccessful()) {
-            $prg++;
-            $cnt++;
-            $percent = intval($prg / $tot * 100) . "%";
-            echo '<script>parent.document.getElementById("progressbar").innerHTML="<div style=\"width:' . $percent
-              . ';background:linear-gradient(to bottom, rgb(6, 80, 6) 0%,rgb(6, 80, 6) 100%);\">&nbsp;</div>";parent.document.getElementById("information").innerHTML="<div style=\"text-align:center; font-weight:bold\">'
-              . $percent . ' is processed.</div>";</script>';
+        while (($emapData = fgetcsv($file, 10000, ",")) !== FALSE) {
+          $fnm = getFirstName($emapData[0]);
+          $phn = $emapData[1];
+          $wrd = $emapData[2];
+          $lga = $emapData[3];
 
-            ob_flush();
-            // flush();
+          $cCrt = canCreate($phn, $db);
+          if (!$cCrt) {
+            $msg .= 'Duplicate Phonenumber (<b>' . $phn . '</b>)!<br>';
+          } else {
+            if ($db->isLastQuerySuccessful()) {
+              $prg++;
+              $cnt++;
+              $percent = intval($prg / $tot * 100) . "%";
 
-            $con = $db->connect();
-            $sql = "INSERT INTO ele_details (ele_Name,ele_Number,ele_Ward,ele_LGA) VALUES (:eleN,:eleM,:eleW:eleL)";
+              echo '<script>parent.document.getElementById("progressbar").innerHTML="<div style=\"width:' . $percent
+                . ';background:linear-gradient(to bottom, rgb(6, 80, 6) 0%,rgb(6, 80, 6) 100%);\">&nbsp;</div>";parent.document.getElementById("information").innerHTML="<div style=\"text-align:center; font-weight:bold\">'
+                . $percent . ' is processed.</div>";</script>';
 
-            $stmt = $con->prepare($sql);
-            $stmt->bindparam(":eleN", $fnm, PDO::PARAM_STR);
-            $stmt->bindparam(":eleM", $phn, PDO::PARAM_STR);
-            $stmt->bindparam(":eleW", $wrd, PDO::PARAM_STR);
-            $stmt->bindparam(":eleL", $lga, PDO::PARAM_STR);
-            $row = $stmt->execute();
+              ob_flush();
+              // flush();
 
-            if ($row) {
-              $msg .= "The Contact <b>" . $fnm . "</b> (" . $phn . ") has been created!<br>";              
+              $con = $db->connect();
+              $sql = "INSERT INTO ele_details (ele_Name,ele_Number,ele_Ward,ele_LGA) VALUES (:eleN,:eleM,:eleW,:eleL)";
+
+              $stmt = $con->prepare($sql);
+              $stmt->bindparam(":eleN", $fnm, PDO::PARAM_STR);
+              $stmt->bindparam(":eleM", $phn, PDO::PARAM_STR);
+              $stmt->bindparam(":eleW", $wrd, PDO::PARAM_STR);
+              $stmt->bindparam(":eleL", $lga, PDO::PARAM_STR);
+              $row = $stmt->execute();
+
+
+              if ($row) {
+                $msg .= "The Contact <b>" . $fnm . "</b> (" . $phn . ") has been created!<br>";
+              }
             }
           }
         }
+      } else {
+        trigger_error('File is unreadable:Please Upload CSV File', E_USER_NOTICE);
       }
     } else {
-      trigger_error('Invalid File:Please Upload CSV File',E_USER_NOTICE);
+      trigger_error('Invalid File:Please Upload CSV File or File is empty', E_USER_NOTICE);
     }
     $db->closeConnection();
   } catch (Exception $e) {
-    trigger_error($db->connectionError());
+    trigger_error($e->getMessage(), E_USER_NOTICE);
   }
 }
 
 function getFirstName($str)
 {
-  $rtn='';
-  $rtn=explode(' ',trim($str));
+  $rtn = '';
+  $rtn = explode(' ', trim($str));
   return ucwords($rtn[0]);
 }
 
-function canCreate($ky,$obj)
+function canCreate($ky, $obj)
 {
-  $rtn=true;
+  $rtn = true;
   if ($obj->isLastQuerySuccessful()) {
     $con = $obj->connect();
     $sql = "SELECT * FROM ele_details WHERE ele_Number LIKE :eleM";
@@ -79,9 +87,13 @@ function canCreate($ky,$obj)
     $stmt->bindparam(":eleM", $ky, PDO::PARAM_STR);
     $stmt->execute();
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $row=$stmt->fetch();
-    if ($row){$rtn=false;}
-  } else {$rtn=false;}
+    $row = $stmt->fetch();
+    if ($row) {
+      $rtn = false;
+    }
+  } else {
+    $rtn = false;
+  }
   return $rtn;
 }
 
@@ -100,7 +112,7 @@ function canCreate($ky,$obj)
           <button class="btn btn-success" type="submit" id="upload" name="upload">Upload</button><br><br></span>
       </form>
       <div id="progressbar" style="border:1px solid #ccc; border-radius: 5px; text-align:left; height:35px;"></div>
-      <div id="information" styel="text-align:center; font-weight:bold;"><?php if (isset($msg) && strlen($msg)>1){ echo $msg; }?></div>
+      <div id="information" styel="text-align:center; font-weight:bold;"><?php if (isset($msg) && strlen($msg) > 1) { echo $msg; } ?></div>
       <iframe id="loadarea" style="display:none;"></iframe><br />
     </div>
   </div>
